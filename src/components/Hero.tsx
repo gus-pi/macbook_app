aed812c14a5c0a8301d879df773c19a2fdc4ed46
PieMenard <pinedo.m.gustavo@gmail.com>
2026-05-08 15:03:23 -0400

    add ref to video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 <h1>MacBook Pro</h1>
                 <img src="/title.png" alt="MacBook Title" />
             </div>
-            <video src="/videos/hero.mp4" autoPlay muted playsInline />
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
             <button>Buy</button>
             <p>From $1599 or $133/month for 12 months</p>
         </section>
